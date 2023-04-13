f490759a4e0983bb83bbe3249ed28ceeb622876b
Noor El-Din Walid <noorwalid72@yahoo.com>
2023-04-13 02:16:42 +0200

    Lay out the foundation for the app.

---
+++ Clean-MVP-News/Utilities/AppConstants.swift
@@ -0,0 +1,8 @@
+//
+//  AppConstants.swift
+//  Clean-MVP-News
+//
+//  Created by Noor Walid on 13/04/2023.
+//
+
+import Foundation
---
+++ Clean-MVP-News/Utilities/AppStrings.swift
@@ -0,0 +1,8 @@
+//
+//  AppStrings.swift
+//  Clean-MVP-News
+//
+//  Created by Noor Walid on 13/04/2023.
+//
+
+import Foundation
